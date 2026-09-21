220522d4de6df4159f4c8e4e9b7e1dd5436f1aea
Teague Sterling <teaguesterling@gmail.com>
2026-09-20 21:23:00 -0700

    fix: update register_helper.hpp for plinking_duck

--- src/include/register_helper.hpp
+++ src/include/register_helper.hpp
@@ -1,47 +1,70 @@
 #pragma once
 
+#include "duckdb/function/scalar_function.hpp"
+#include "duckdb/function/table_function.hpp"
 #include "duckdb/main/extension/extension_loader.hpp"
 #include "duckdb/parser/parsed_data/create_scalar_function_info.hpp"
 #include "duckdb/parser/parsed_data/create_table_function_info.hpp"
-#include "duckdb/function/function.hpp"
-#include "duckdb/function/scalar_function.hpp"
-#include "duckdb/function/table_function.hpp"
 
 namespace duckdb {
 
-inline void RegisterScalarWithDesc(ExtensionLoader &loader, ScalarFunction func, vector<string> parameter_names,
-                                   string description, vector<string> examples = {}) {
-	FunctionDescription desc(parameter_names, description, examples, {"plinking_duck"});
-	func.descriptions.push_back(std::move(desc));
-	auto info = make_uniq<CreateScalarFunctionInfo>(std::move(func));
-	info->on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+inline void RegisterScalarWithDesc(ExtensionLoader &loader, ScalarFunction fn,
+                                   const vector<string> &params = {},
+                                   const string &desc_str = "",
+                                   const vector<string> &examples = {}) {
+	CreateScalarFunctionInfo info(std::move(fn));
+	info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+	FunctionDescription desc;
+	desc.parameter_names = params;
+	desc.description = desc_str;
+	desc.examples = examples;
+	desc.categories = {"plinking_duck"};
+	info.descriptions.push_back(desc);
 	loader.RegisterFunction(std::move(info));
 }
 
-inline void RegisterScalarSetWithDesc(ExtensionLoader &loader, ScalarFunctionSet set, vector<string> parameter_names,
-                                      string description, vector<string> examples = {}) {
-	FunctionDescription desc(parameter_names, description, examples, {"plinking_duck"});
-	set.descriptions.push_back(std::move(desc));
-	auto info = make_uniq<CreateScalarFunctionInfo>(std::move(set));
-	info->on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+inline void RegisterScalarSetWithDesc(ExtensionLoader &loader, ScalarFunctionSet fn_set,
+                                      const vector<string> &params = {},
+                                      const string &desc_str = "",
+                                      const vector<string> &examples = {}) {
+	CreateScalarFunctionInfo info(std::move(fn_set));
+	info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+	FunctionDescription desc;
+	desc.parameter_names = params;
+	desc.description = desc_str;
+	desc.examples = examples;
+	desc.categories = {"plinking_duck"};
+	info.descriptions.push_back(desc);
 	loader.RegisterFunction(std::move(info));
 }
 
-inline void RegisterTableWithDesc(ExtensionLoader &loader, TableFunction func, vector<string> parameter_names,
-                                  string description, vector<string> examples = {}) {
-	FunctionDescription desc(parameter_names, description, examples, {"plinking_duck"});
-	func.descriptions.push_back(std::move(desc));
-	auto info = make_uniq<CreateTableFunctionInfo>(std::move(func));
-	info->on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+inline void RegisterTableWithDesc(ExtensionLoader &loader, TableFunction fn,
+                                  const vector<string> &params = {},
+                                  const string &desc_str = "",
+                                  const vector<string> &examples = {}) {
+	CreateTableFunctionInfo info(std::move(fn));
+	info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+	FunctionDescription desc;
+	desc.parameter_names = params;
+	desc.description = desc_str;
+	desc.examples = examples;
+	desc.categories = {"plinking_duck"};
+	info.descriptions.push_back(desc);
 	loader.RegisterFunction(std::move(info));
 }
 
-inline void RegisterTableSetWithDesc(ExtensionLoader &loader, TableFunctionSet set, vector<string> parameter_names,
-                                     string description, vector<string> examples = {}) {
-	FunctionDescription desc(parameter_names, description, examples, {"plinking_duck"});
-	set.descriptions.push_back(std::move(desc));
-	auto info = make_uniq<CreateTableFunctionInfo>(std::move(set));
-	info->on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+inline void RegisterTableSetWithDesc(ExtensionLoader &loader, TableFunctionSet fn_set,
+                                     const vector<string> &params = {},
+                                     const string &desc_str = "",
+                                     const vector<string> &examples = {}) {
+	CreateTableFunctionInfo info(std::move(fn_set));
+	info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+	FunctionDescription desc;
+	desc.parameter_names = params;
+	desc.description = desc_str;
+	desc.examples = examples;
+	desc.categories = {"plinking_duck"};
+	info.descriptions.push_back(desc);
 	loader.RegisterFunction(std::move(info));
 }
 
